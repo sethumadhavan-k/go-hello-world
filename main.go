afe039880f6ccfc2e2ddeeb05f4eff9f85e5198f
Sethumadhavan K <sethuksethu@gmail.com>
2024-02-20 18:41:42 +0530

    new api

--- main.go
+++ main.go
@@ -10,11 +10,18 @@ func handler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, "Hello World")
 }
 
+
+func printName(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, "<your-name>")
+}
+
+
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 
 	router := gin.Default()
 	router.GET("/", handler)
+	router.GET("/", printName)
 
 	fmt.Println("Go REST service started...")
 	router.Run("0.0.0.0:9009")
