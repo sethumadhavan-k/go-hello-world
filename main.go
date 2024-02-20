3abdde6a40e4d274ae36c42158325a7b5dddeb78
Sethumadhavan K <sethuksethu@gmail.com>
2024-02-20 18:47:06 +0530

    added new timer handler

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func handler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, "Hello World")
+}
+
+func printTime(ctx *gin.Context) {
+	t := time.Now()
+	ctx.JSON(http.StatusOK, t.Format("20060102150405"))
 }
 
 func main() {
@@ -15,6 +21,7 @@ func main() {
 
 	router := gin.Default()
 	router.GET("/", handler)
+	router.GET("/time", printTime)
 
 	fmt.Println("Go REST service started...")
 	router.Run("0.0.0.0:9009")
